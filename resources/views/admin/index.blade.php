2fda882f016feee288741f21ff32f4d98c3f2035
Rayen Dammak <dammakrayyen@gmail.com>
2025-09-30 12:29:04 +0100

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -19,19 +19,7 @@
           </div>
         </div>
       </div>
-      <div class="py-12">
-        <div class="max-w-7xl mx-auto">
-          <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <div class="p-4">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    {{ 'List of Clients' }}
-                    <br><br>
-                    <a href="{{ route('posts.create') }}" class="btn btn-primary ml-2">Add Clients</a>
-                </h2>
-            </div>
-          </div>
-        </div>
-      </div>
+      
       <div class="py-12">
         <div class="max-w-7xl mx-auto">
           <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
