c9f08c304dd762c4766f5dc599fb606ec5446a36
Rayen Dammak <dammakrayyen@gmail.com>
2025-09-30 09:30:14 +0100

    Update create.blade.php

--- resources/views/tournois/create.blade.php
+++ resources/views/tournois/create.blade.php
@@ -1,20 +1,20 @@
 <x-app-layout>
     <br>
-<h1 class="text-center text-3xl font-bold" >Nouvelle Réparation</h1>
+<h1 class="text-center text-3xl font-bold" >New Connection</h1>
 <div class="container my-5">
 <form method="Post" action="{{route('tournois.store')}}">
     @csrf
-    <div class="form-group " ><label for="nom">Marque de l'appareil</label>
+    <div class="form-group " ><label for="nom">Cloud Provider Name</label>
         <input class="form-control" name=nom id="nom" type="text" value="{{old('nom')}}"></div>
-    <div><label for="localisation">Modèle de l'appareil</label>
+    <div><label for="localisation">Startup Name</label>
         <input class="form-control" name="localisation" id="localisation" type="text" value="{{old('localisation')}}"></div>
-    <div  ><label for="date">Type de panne </label>
+    <div  ><label for="date">Subscription Expiration Date </label>
         <input class="form-control" name="date" id="date" type="text" value="{{old('date')}}"></div>
-    <div><label for="frais">Prix à estimer</label>
+    <div><label for="frais">Phone Number</label>
             <input class="form-control" name="frais" id="frais" type="text" value="{{old('frais')}}"></div>
-    <div><label for="prix">code de réparation </label>
+    <div><label for="prix">API Key </label>
         <input class="form-control" name="prix" id="prix" type="text" value="{{old('prix')}}"></div>
-    <div><label for="nombrejoueurs">Numéro de telephone joignable</label>
+    <div><label for="nombrejoueurs">Account ID</label>
             <input class="form-control" name="nombrejoueurs" id="nombrejoueurs" type="text" value="{{old('nombrejoueurs')}}"></div>
         @if($errors->any())
         <ul>
@@ -24,8 +24,8 @@
         </ul>
         @endif
         <div class="d-flex justify-content-center mt-3">
-            <button class="btn  btn-primary" type="submit"> Confirmer </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <a class="btn btn-secondary" href="{{route('dashboard')}}">Retourner</a>
+            <button class="btn  btn-primary" type="submit"> Add Connection </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a class="btn btn-secondary" href="{{route('dashboard')}}">Go Back</a>
         
         </div>
 </form>
