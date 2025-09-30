b0b21bd05e1ec17a2d20cbc52ef1214d1bf25cd2
Rayen Dammak <dammakrayyen@gmail.com>
2025-09-30 13:59:21 +0100

    Update tennishub.blade.php

--- resources/views/tennishub.blade.php
+++ resources/views/tennishub.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ONE CLOUDIUM </title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    
   
     @vite(['resources/css/rayen.css'])
 
